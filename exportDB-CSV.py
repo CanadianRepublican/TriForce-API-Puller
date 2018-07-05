452952370bcf207a0b1b7c9c6e6a1bc0e02df758
Rahim Khoja <CanadianRepublican@users.noreply.github.com>
2018-07-04 17:54:46 -0700

    Update exportDB-CSV.py

--- exportDB-CSV.py
+++ exportDB-CSV.py
@@ -14,6 +14,6 @@ cur=db.cursor()
 cur.execute(QUERY)
 result=cur.fetchall()
 
-c = csv.writer(open('dbdump01.csv', 'wb'))
+c = csv.writer(open('daily-csv-export-'+timestr+'.csv', 'wb'))
 for x in result:
 c.writerow(x)
