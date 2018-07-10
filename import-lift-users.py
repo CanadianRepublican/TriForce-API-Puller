ebc3ac720da7ce82528c26b8736256d51ae36191
Rahim Khoja <CanadianRepublican@users.noreply.github.com>
2018-07-10 13:01:36 -0700

    Update import-lift-users.py

--- import-lift-users.py
+++ import-lift-users.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 
-try:
-    # For Python 3.0 and later
-    from urllib.request import urlopen
-except ImportError:
-    # Fall back to Python 2's urllib2
-    from urllib2 import urlopen
-
+from urllib.request import urlopen
 import json
 import MySQLdb
 import datetime
